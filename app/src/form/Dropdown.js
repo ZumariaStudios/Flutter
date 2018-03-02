e1a53e5395703f4143e669c0478f2213f424e4ce
Michael Bowman <bowmanmc@gmail.com>
2018-03-02 09:14:49 -0500

    dropdown tweaks

--- app/src/form/Dropdown.js
+++ app/src/form/Dropdown.js
@@ -6,22 +6,24 @@ const Dropdown = (props) => {
 
     let opts = [];
     props.data.map(datum => {
-      opts.push({
-        value: datum,
-        label: datum
-      });
-      return false;
+        opts.push({
+            value: datum,
+            label: datum
+        });
+        return false;
     });
 
     return (
-      <Select
-        name={props.name}
-        value={props.value}
-        onChange={(evt) => {
-          props.onChange(evt.value);
-        }}
-        options={opts}
-      />
+        <div className="Dropdown">
+            <Select
+                name={props.name}
+                value={props.value}
+                onChange={(evt) => {
+                    props.onChange(evt.value);
+                }}
+                options={opts}
+            />
+        </div>
     );
 }
 
