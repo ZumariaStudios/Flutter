c521b95ab44d2b6faa0bc85a2fd519073f327cfe
Amberly <ahoffman@miletwo.us>
2018-03-06 13:00:26 -0500

    Need to link to local img

--- app/src/form/Header.js
+++ app/src/form/Header.js
@@ -2,13 +2,13 @@ import React from 'react';
 import '../defaults/_header.scss';
 
 const Header = (props) => {
+let img = new Image();
   return (
-    <div>
-       <div class="headerComponenet">
-        <img alt="FPO Image" src={props.src} />
+       <div id="headerComponenet">
+        <img alt="FPO Image" src={props.imgSrc} />
+        <h1>{props.header}</h1>
        </div>
-          <h1>{props.header}</h1>
-    </div>
+
 );
 }
 export default Header;
