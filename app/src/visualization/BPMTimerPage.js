d405bc31443934889b5023ea65f644ebfb3ac1e8
Amberly <ahoffman@miletwo.us>
2018-03-09 13:23:28 -0500

    Countdown and buttons done

--- app/src/visualization/BPMTimerPage.js
+++ app/src/visualization/BPMTimerPage.js
@@ -6,7 +6,12 @@ import Header from '../form/Header.js';
 class BPMTimerPage extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {count: 15}
+    this.state = {
+      count: 15,
+      show: false,
+    }
+
+    this.startTimer.bind(this);
   }
   componentWillUnmount () {
     clearInterval(this.timer)
@@ -23,17 +28,24 @@ class BPMTimerPage extends React.Component {
     clearInterval(this.timer)
   }
 
-
   render () {
     return (
       <div className='timer'>
         <Header header = "Press start when you're ready" />
+
         <h1>{this.state.count}</h1>
-            <button onClick={this.startTimer.bind(this)}>Start</button>
-        <input class="startTimer"
-         type='submit'
-         value='Continue'
-         onClick={this.nextSection}/>
+        {(this.state.count > 0) ?
+          <input class="startTimer"
+           type='submit'
+           value='Start'
+           onClick={(e) => this.startTimer(e)}/>
+           : null}
+        {this.state.count === 0 ?
+            <input class="startTimer"
+             type='submit'
+             value='Continue'
+             onClick={this.nextSection}/>
+           : null}
       </div>
     );
   }
