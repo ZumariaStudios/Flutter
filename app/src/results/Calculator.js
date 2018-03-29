d4d23db2e31fbe006dadccd5ad10b5ba5c2d2628
Amberly <ahoffman@miletwo.us>
2018-03-29 16:42:41 -0400

    end working day Thursday

--- app/src/results/Calculator.js
+++ app/src/results/Calculator.js
@@ -88,10 +88,24 @@ export const calcFraminghamBMIModel = () => {
       + diabeticContribution
       + sbpContribution;
 
+
+
+
+
   // Determine risk
-  let riskStatus = Math.round(100 * (1 - Math.pow(expBase, Math.exp(totalContributions))));
+  let riskStatus = Math.round (1 - Math.pow(expBase, Math.exp(totalContributions)));
   return riskStatus;
 }
+
+
+
+
+
+
+
+
+
+
 
 export const calcFraminghamLipidModel = () => {
 
